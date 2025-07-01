197ea0f69ab9f8a7d5cae47a2ca9a39dbaeecb49
Luis Añazgo A <luis.adhemir.develop@gmail.com>
2025-07-01 15:26:49 -0500

    primer commit

---
+++ src/test/java/pe/com/servicio/mantenimiento/MantenimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.servicio.mantenimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantenimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
